0cac6ef69015c6d74b8f84ee945a12ad916ccdb0
adralcan <adralcan@ucm.es>
2017-12-29 05:00:52 +0100

    queda por comentar CollisionResolution_Ground y luego arreglar el codigo para que quede bonito

--- freeglut project 3D/Physics_Functions.cpp
+++ freeglut project 3D/Physics_Functions.cpp
@@ -7,8 +7,8 @@ Vector3d (*acc_func)(State); //acceleration function pointer
 Particle3d *P;	//array of particles to reference(p3d or pp3d)
 State *PS;
 short P_C;
-Vector3d ContactNormal;
-double b_g=6.67384*0.000080, s_g;
+Vector3d ContactNormal; // Vector de la normal cuando dos objetos se colisionan
+double b_g=6.67384*0.000080, s_g; // el 6.67384 es la constante de gravitación universal
 const short NumEstrellas = 1000;
 const short NumParticulas = 125;
 vector<Particle3d> particulas(NumParticulas);
@@ -25,8 +25,12 @@ Vector3d accUniversalGravitation(State ip)
 	
 	for(int i=0; i<P_C; i++)
 	{
+		// Salta a la siguiente iteracion
+		// Supongo que es para ahorrarse comprobar con la propuesta y las anteriores.
+		//SUDA, SE CAMBIA ESTO Y FUERA
 		if(ip.id==i) continue;
 		
+		// supongo que distancia
 		double d=sqrt( (ip.Position.x-P[i].position.x)*(ip.Position.x-P[i].position.x) +
 				 (ip.Position.y-P[i].position.y)*(ip.Position.y-P[i].position.y) +
 				 (ip.Position.z-P[i].position.z)*(ip.Position.z-P[i].position.z)
@@ -50,6 +54,7 @@ Vector3d accDirectionalGravity(State sip)
 	return Vector3d(0,-10,0);
 }
 
+// evaluar es que le pasas un indice y le aplica las ecuaciones a la posicion y velocidad.
 Derivative evaluate(int ip)
 {
 	Derivative output;
@@ -59,14 +64,15 @@ Derivative evaluate(int ip)
 	return output;
 }
 
-
+// Sobrecarga de la evaluacion, una forma mas compleja
 Derivative evaluate(int ip, float dt, Derivative &d)
 {
 	State state1, state2;
 	Derivative output;
 	state1.id=ip;
 	state1.mass=P[ip].mass;
-	state1.Position = P[ip].position + d.dPosition*dt + d.dVelocity*0.5*dt*dt;// dp/dt= velocity
+	// dp (derivada de la aceleracion), dt (derivada del tiempo)
+	state1.Position = P[ip].position + d.dPosition*dt + d.dVelocity*0.5*dt*dt;
 	state1.Velocity = P[ip].velocity + d.dVelocity*dt;//p''=dv/dt= acceleration
 	
 	output.dPosition = state1.Velocity;
@@ -75,6 +81,7 @@ Derivative evaluate(int ip, float dt, Derivative &d)
 	return output;
 }
 
+// No se usa en la demo
 void DirectionalGravity_simple(int ip, Particle3d* p , short p_c, State& fstate, float dt)
 {
 	P=p;
@@ -95,66 +102,63 @@ void DirectionalGravity_simple(int ip, Particle3d* p , short p_c, State& fstate,
 
 }
 
-
-
+//SE USA CUANDO PULSAS CLICK IZQUIERDO
 void DirectionalGravity(int ip, Particle3d* p , short p_c, State& fstate, float dt)
 {
-	P=p;
-	P_C=p_c;
-	acc_func=accDirectionalGravity;
-	Derivative k1 = evaluate(ip);
-	Derivative k2 = evaluate(ip, dt*0.5f, k1);
-	Derivative k3 = evaluate(ip, dt*0.5f, k2);
-	Derivative k4 = evaluate(ip, dt, k3);
+	P = p;
+	P_C = p_c;
+	acc_func = accDirectionalGravity;
 
-	Vector3d dPOSITION;
-	Vector3d dVELOCITY;
+	// Menudos triples me voy a marcar ahora, atentos
+	Derivative k1 = evaluate(ip); // determina la curva de la funcion
+	Derivative k2 = evaluate(ip, dt * 0.5f, k1); // creo que son los puntos de inflexion
+	Derivative k3 = evaluate(ip, dt * 0.5f, k2); // es una constante
+	Derivative k4 = evaluate(ip, dt, k3); // tiene que dar 0 para que sea correcto
 
-	dPOSITION= 1.0f/6.0f*(k1.dPosition + 2.0f*(k2.dPosition + k3.dPosition) + k4.dPosition);
-	dVELOCITY = 1.0f/6.0f*(k1.dVelocity + 2.0f*(k2.dVelocity + k3.dVelocity) + k4.dVelocity);
-	
-	
-	fstate.Position += dPOSITION*dt; 
-	fstate.Velocity += dVELOCITY*dt;
-	fstate.Acceleration = dVELOCITY*dt;
+	Vector3d dPOSITION = 1.0f / 6.0f * (k1.dPosition + 2.0f * (k2.dPosition + k3.dPosition) + k4.dPosition);
+	Vector3d dVELOCITY = 1.0f / 6.0f * (k1.dVelocity + 2.0f * (k2.dVelocity + k3.dVelocity) + k4.dVelocity);
+
+	fstate.Position += dPOSITION * dt;
+	fstate.Velocity += dVELOCITY * dt;
+	fstate.Acceleration = dVELOCITY * dt;
 
 }
 
+// Solo tiene una diferencia con el metodo anterior
 void UniversalGravitation(int ip, Particle3d* p, short p_c, State& fstate , float dt, double G )
 {
-	P=p;
-	P_C=p_c;
-	
-	if(G>=0)
-	b_g=G;
+	P = p;
+	P_C = p_c;
 
-	acc_func=accUniversalGravitation;
+	// Esta es la unica diferencia
+	if (G >= 0)
+		b_g = G;
+
+	// igual que en el metodo de arriba
+	acc_func = accUniversalGravitation;
 	Derivative k1 = evaluate(ip);
-	Derivative k2 = evaluate(ip, dt*0.5f, k1);
-	Derivative k3 = evaluate(ip, dt*0.5f, k2);
+	Derivative k2 = evaluate(ip, dt * 0.5f, k1);
+	Derivative k3 = evaluate(ip, dt * 0.5f, k2);
 	Derivative k4 = evaluate(ip, dt, k3);
 
-	Vector3d dPOSITION;
-	Vector3d dVELOCITY;
+	Vector3d dPOSITION = 1.0f / 6.0f*(k1.dPosition + 2.0f*(k2.dPosition + k3.dPosition) + k4.dPosition);
+	Vector3d dVELOCITY = 1.0f / 6.0f*(k1.dVelocity + 2.0f*(k2.dVelocity + k3.dVelocity) + k4.dVelocity);
 
-	dPOSITION= 1.0f/6.0f*(k1.dPosition + 2.0f*(k2.dPosition + k3.dPosition) + k4.dPosition);
-	dVELOCITY = 1.0f/6.0f*(k1.dVelocity + 2.0f*(k2.dVelocity + k3.dVelocity) + k4.dVelocity);
-
-	
-	fstate.Position += dPOSITION*dt; 
+	fstate.Position += dPOSITION*dt;
 	fstate.Velocity += dVELOCITY*dt;
 	fstate.Acceleration = dVELOCITY*dt;
-	
-
 }
 
+// Literalmente lo mismo que DirectionalGravity_simple, excepto por (*)
 void UniversalGravitation_simple(int ip, Particle3d* p, short p_c, State& fstate , float dt, double G )
 {
 	P=p;
 	P_C=p_c;
 	
-	if(G>=0)
-	b_g=G;
+	// (*)
+	if (G >= 0)
+		b_g = G;
+	// (*)
 
 	acc_func=accUniversalGravitation;
 	Derivative k1 = evaluate(ip);
@@ -170,7 +174,7 @@ void UniversalGravitation_simple(int ip, Particle3d* p, short p_c, State& fstate
 	fstate.Acceleration = dVELOCITY*dt;
 
 }
-void CollisionResolution2(int p_c, Particle3d *p, State *ps)
+/*void CollisionResolution2(int p_c, Particle3d *p, State *ps)
 {
 	Vector3d Normal, Tangent, Tangent2, Tangent3;
 	double ra, rb;
@@ -229,72 +233,77 @@ void CollisionResolution2(int p_c, Particle3d *p, State *ps)
 		}
 	
 
-}
+}*/
 
+// Se utiliza cuando va a colisionar un objeto con otro
 void dImpenetrationResolution(int i, int j, Particle3d *p, State *ps)
 {
+	// previous distances i & j
+	double di = Distance3d(p[i].position, ps[i].Position);
+	double dj = Distance3d(p[j].position, ps[j].Position);
 
-				
-	double di=Distance3d(p[i].position, ps[i].Position);//previous distances
-	double dj=Distance3d(p[j].position, ps[j].Position);
-	double err_l=abs((p[i].radius+p[j].radius)-Distance3d(p[i].position, p[j].position));
+	// Comprueba si las particulas estan separadas, solapadas o pegandose.
+	// Es como una zona muerta
+	double err_l = abs((p[i].radius + p[j].radius) - Distance3d(p[i].position, p[j].position));
 
-								double vm1=p[i].velocity.Magnitude(), vm2=p[j].velocity.Magnitude();
-								double tv=1.0/(p[j].mass+vm2) + 1.0/(p[i].mass+vm1);
-								//double tv=vm1 + vm2;
-								
-						Vector3d mv=ContactNormal;
-						mv.Normalize();
-						
-						mv  *=(-err_l / (tv));
-							ps[i].Position+= mv*(1/(-p[i].mass+vm1)),
-							ps[j].Position+= mv*(1/(p[j].mass+vm2));	
-						
-							
+	// dos variables que son las magnitudes de las coordenadas de la particula de la particula
+	double vm1 = p[i].velocity.Magnitude(), vm2 = p[j].velocity.Magnitude();
+	
+	// ???????????????????
+	double tv = 1.0 / (p[j].mass + vm2) + 1.0 / (p[i].mass + vm1);
+		
+	
+	Vector3d mv = ContactNormal;
+	mv.Normalize();
 
+	// a la normal de la colision le multilpicamos el error entre
+	mv *= (-err_l / tv);
 
-					
+	ps[i].Position += mv * (1 / (-p[i].mass+vm1)),
+	ps[j].Position += mv * (1 / (p[j].mass+vm2));				
 }
 
 void CollisionResolution(int p_c, Particle3d *p, State *ps)
 {
-	double d1, d2;
-	double ra, rb;
+	double d1, d2; // estas seran las deltas de las funciones
+	double ra, rb; // supongo que son los radios de las esferas
 	double err_l;
-	double C=0.60;
+	double C = 0.60;
 	Vector3d Normal, Tangent, Tangent2, Tangent3;
 
-		for(int i=0; i<p_c; i++)
-		{
-			for(int j=i; j<p_c; j++)
-			{
-				if(i==j)
-					continue;
+	for (int i = 0; i < p_c; i++) {
+		for (int j = i; j < p_c; j++) {
+			// ponemos el resto del codigo en la condicion y quitamos el continue
+			if(i == j)
+				continue;
 
-				double d=(Distance3d(ps[i].Position, ps[j].Position)-(p[i].radius+p[j].radius));
-				if( d<0)	
-				{
-						
-					Normal.set(ps[i].Position.x - ps[j].Position.x, ps[i].Position.y - ps[j].Position.y, ps[i].Position.z - ps[j].Position.z);
-					Normal.Normalize();
-					ContactNormal=Normal;
-					dImpenetrationResolution(i,j,p,ps);
-											
-					double separatingVelocity = dotProduct(ps[i].Velocity-ps[j].Velocity,Normal);
-					double newSepVelocity = -separatingVelocity * C;
-					double deltaVelocity = newSepVelocity - separatingVelocity;
-											
-					const double totalInverseMass = 1.0/(ps[i].mass)+1.0/(ps[j].mass);
-											
-					// Calculate the impulse to apply.
-					double impulse = deltaVelocity / totalInverseMass;
-					// Find the amount of impulse per unit of inverse mass.
-					Vector3d impulsePerIMass = Normal * impulse;
-					// Apply impulses: they are applied in the direction of the contact,
-					// and are proportional to the inverse mass.
-					ps[i].Velocity=ps[i].Velocity + impulsePerIMass* (1/ps[i].mass);
-					ps[j].Velocity=ps[j].Velocity + impulsePerIMass *(-1/ps[j].mass);
-									
+			// distancia entre los objetos
+			double d = (Distance3d(ps[i].Position, ps[j].Position) - (p[i].radius + p[j].radius));
+			if(d < 0) { 
+				Normal.set(ps[i].Position.x - ps[j].Position.x, ps[i].Position.y - ps[j].Position.y,
+					ps[i].Position.z - ps[j].Position.z);
+				Normal.Normalize();
+				ContactNormal = Normal;
+
+				// Comprobacion de la colision
+				dImpenetrationResolution(i, j, p, ps);
+										
+				double separatingVelocity = dotProduct(ps[i].Velocity-ps[j].Velocity, Normal);
+				double newSepVelocity = -separatingVelocity * C;
+				double deltaVelocity = newSepVelocity - separatingVelocity;
+				
+				// no lo entiendo muy bien la verdad
+				const double totalInverseMass = 1.0/(ps[i].mass) + 1.0/(ps[j].mass);
+										
+				// Calculate the impulse to apply.
+				double impulse = deltaVelocity / totalInverseMass;
+				// Find the amount of impulse per unit of inverse mass.
+				Vector3d impulsePerIMass = Normal * impulse;
+				// Apply impulses: they are applied in the direction of the contact,
+				// and are proportional to the inverse mass.
+				ps[i].Velocity = ps[i].Velocity + impulsePerIMass * (1 / ps[i].mass);
+				ps[j].Velocity = ps[j].Velocity + impulsePerIMass * (-1 / ps[j].mass);
+								
 
 				}
 				
